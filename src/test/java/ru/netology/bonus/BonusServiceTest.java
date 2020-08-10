a6fd5aca5c71878e3428689d4c5769d2405362fd
Tatana <burmatova@inbox.ru>
2020-08-10 16:41:11 +0300

    Initial commit

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.bonus;
 
+import org.junit.jupiter.api.parallel.Resources;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,20 +10,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
-// /Users/tatanaburmatova/Desktop/ПРОГРАМИРОВАНИЕ/НЕТОЛОГИЯ/4. JAVA/ДОМАШКИ/WorkClass_6_ParameterizedTests/src/main/java/ru/netology/bonus/BonusService.java
-// /Users/tatanaburmatova/Desktop/ПРОГРАМИРОВАНИЕ/НЕТОЛОГИЯ/4. JAVA/ДОМАШКИ/WorkClass_6_ParameterizedTests/src/test/java/ru/netology/bonus/BonusServiceTest.java
+
 
 
     @ParameterizedTest
-    @CsvFileSource(value = String[] resources())
-    @CsvSource({
-                    "'registered user, bonus under limit', 100060, true, 30",
-                    "'registered user, bonus over limit', 100000060, true, 500",
-                    "'unregistered user, bonus under limit', 100060, false, 10",
-                    "'unregistered user, bonus over limit', 100000060, false, 500"
-            })
+    @CsvFileSource(resources = "/data.csv")
 
-    void shouldCalculate(String test, long amount, boolean registered, long expected) {
+   void shouldCalculate(String test, long amount, boolean registered, long expected) {
         BonusService bonusService = new BonusService();
 
 //        вызываем целевой метод:
